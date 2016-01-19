a0363384d2b3704dacf9b285088da59e3e239f5a
dgeene <david.geene@gmail.com>
2016-01-19 13:51:55 -0500

    create sdl renderer

--- Lesson1/src/main.cpp
+++ Lesson1/src/main.cpp
@@ -23,4 +23,14 @@ int main()
         std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         return 1;
     }
+
+    //create the renderer
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr)
+    {
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
 }
