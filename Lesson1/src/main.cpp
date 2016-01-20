64989b8679db753a136768bd7c76fa48bbf54c37
dgeene <david.geene@gmail.com>
2016-01-20 08:51:32 -0500

    add better cleanup to Lesson1

--- Lesson1/src/main.cpp
+++ Lesson1/src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <SDL.h>
 #include "res_path.h"
+#include "cleanup.h"
 
 /*
  * Lesson 1: Draw bitmap
@@ -28,7 +29,7 @@ int main()
     SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (ren == nullptr)
     {
-        SDL_DestroyWindow(win);
+        cleanup(win);
         std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
@@ -40,8 +41,7 @@ int main()
     SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
     if (bmp == nullptr)
     {
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
+        cleanup(ren, win);
         std::cout << "SDL LoadBitmap Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
@@ -53,8 +53,7 @@ int main()
     SDL_FreeSurface(bmp);
     if (tex == nullptr)
     {
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
+        cleanup(ren, win);
         std::cout << "SDL CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
@@ -74,8 +73,6 @@ int main()
 
 
 
-    SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
+    cleanup(tex, ren, win);
     SDL_Quit();
 }
